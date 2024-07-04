d3679f42ee05d58ac59071b5acfc3b5e91588a37
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-07-04 03:37:25 -0300

    Componente exibido apenas para usuários autenticados

---
+++ src/app/components/MessageUser.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import React from "react";
+import Button from "./Button";
+
+
+export default function MessageUser() {
+  const { status, data: session } = useSession();
+
+  if (status !== "authenticated") {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center gap-2">
+      <span className="text-white rounded-sm px-2">
+        {`Olá ${
+          session?.user?.name.split(" ")[0] // quando o usuário estiver autenticado ele vai pegar da sessão o primeiro userName
+        }`}
+      </span>
+      <Button
+        text="Sair"
+        className="bg-red-600 text-white rounded px-2 cursor-pointer"
+        onClick={() => signOut()}
+      />
+    </div>
+  );
+}
