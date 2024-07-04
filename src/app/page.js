886f8948797981bbc1e3d49ddddbdc3d065cbe7f
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-07-04 13:20:29 -0300

    Feat: novo componete na tela principal

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 export default function Home() {
 
   return (
     //<LayoutAdmin> 
     <main className="min-h-screen flex flex-col items-center pt-6">
+      <Header/>
       <div className="text-center mb-6 text-2xl">Só Cozinho Verde</div>
     </main>
   );
