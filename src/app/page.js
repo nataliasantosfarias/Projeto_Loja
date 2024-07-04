08104a9f28f8796d48b3c4f8ff4b2f5d49c8287c
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-07-04 02:12:01 -0300

    STYLE: Ajusta a mensagem na tela principal

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
-import LayoutAdmin from "/src/app/components/LayoutAdmin.jsx";
+
+import Link from 'next/link';
+import Button from './components/Button';
 
 export default function Home() {
+
   return (
-     <LayoutAdmin> 
-      <main className="min-h-screen flex justify-center pt-6">Home nova</main>
-     </LayoutAdmin> 
+    //<LayoutAdmin> 
+    <main className="min-h-screen flex flex-col items-center pt-6">
+      <div className="text-center mb-6">Só cozinho Verde</div>
+
+
+        {/* Botão login */}
+        <Link href="/login">
+          <Button text="Cadastro"
+            className="w-40 bg-green-600 text-white rounded px-2 cursor-pointer mb-4"
+          />
+        </Link>
+        <Link href="/register">
+          <Button text="Login"
+            className="w-40 bg-green-600 text-white rounded px-2 cursor-pointer mb-4"
+          />
+        </Link>
+    </main>
   );
 }
