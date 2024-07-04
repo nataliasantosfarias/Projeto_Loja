eb9f2637a8d20207f81bf8fdef99738d16821573
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-07-04 13:34:11 -0300

    STYLE: Estiliza página home

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
+import RegisterLoginButtons from "./components/RegisterLoginButtons";
+import MessageUser from "./(private)/MessageUser";
+
 export default function Home() {
 
   return (
     //<LayoutAdmin> 
     <main className="min-h-screen flex flex-col items-center pt-6">
-      <Header/>
-      <div className="text-center mb-6 text-2xl">Só Cozinho Verde</div>
+      <Header />
+      <MessageUser />
+      <RegisterLoginButtons />
     </main>
   );
 }
