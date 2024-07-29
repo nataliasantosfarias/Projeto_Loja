804ffeb6b5e023d272d19283add324847d028537
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-07-29 03:53:41 -0300

    Fix: resolução de bug no deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/index.html", /**new */
+    // "./public/index.html", /**new */
   ],
 
 
