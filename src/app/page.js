177a5873394db87712847b5ac835d4a0d47d459c
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-07-29 02:56:51 -0300

    Style: Adiciona imagens

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,20 @@ import React from "react";
 export default function Home() {
 
   return (
-    
-     <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div class="md:flex">
-          <div class="md:shrink-0">
-            <img class="h-48 w-full object-cover md:h-full md:w-48" src="/public/img/verde.svg" alt="Verde"/>
-        </div>
-      </div>
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+      <img className="w-full object-cover" src="/img/img1.png" alt="Minha Imagem" />
     </div>
+
+
+    //  <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    //     <div class="md:flex">
+          
+    //       <div className="md:shrink-0">
+            
+    //       <img src="/img/img1.png" alt="Minha Imagem" />
+    //     </div>
+    //   </div>
+    // </div>
     
       
 
