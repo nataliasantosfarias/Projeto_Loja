58b573ef569339187493233bc51a88dbdf523f93
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-07-30 02:46:26 -0300

    fix: deploy input

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Button from "@/app/components/Button"
-import Input from "@/app/components/Input";
+import Input from "../../components/Input"; 
 import Link from "next/link";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
