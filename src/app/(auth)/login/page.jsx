3aa74a61111d9567f999bc0c50d16a955c0f1120
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-07-30 14:03:09 -0300

    Fix: deploy

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,8 +1,8 @@
 "use client"; //Informa que será uma página usada pelo cliente
 import { Form, Formik } from "formik";
 import React, { useEffect,useState } from "react";
-import Input from "../../components/Input"; 
-import Button from "../../components/Button"; //Importa o componente Button
+import Input from ".@/app/components/Input"; 
+import Button from "@/components/Button"; //Importa o componente Button
 import Link from "next/link";
 import * as Yup from "yup"; //validação
 import { signIn, useSession } from "next-auth/react";
